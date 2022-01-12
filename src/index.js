93f67b4a4e08af303315e167d3649271197b6aba
tariktesfa <tariktesfa9090@gmail.com>
2022-01-12 15:55:10 +0300

    Add the list and finish styling

--- src/index.js
+++ src/index.js
@@ -1,13 +1,3 @@
+// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
