8469c356ac845b5d652e673c4376fe0156112751
tariktesfa <tariktesfa9090@gmail.com>
2022-01-12 17:40:01 +0300

    Add populate function to the to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-// eslint-disable-next-line no-unused-vars
-import _ from 'lodash';
 import './style.css';
+
+const taskContainer = document.querySelector('.list-container');
+
+const tasks = [
+  {
+    description: 'Finishing yesterday activities',
+    index: 0,
+    completed: true,
+  },
+  {
+    description: 'Reading book',
+    index: 1,
+    completed: false,
+  },
+  {
+    description: 'Calling to my mom',
+    index: 2,
+    completed: false,
+  },
+  {
+    description: 'Go for shopping',
+    index: 3,
+    completed: true,
+  },
+];
+
+const populateTasks = () => {
+  tasks.forEach((lists) => {
+    const taskListDiv = document.createElement('div');
+    taskListDiv.classList.add('list-items');
+    taskListDiv.id = lists.index;
+    taskListDiv.innerHTML = `
+    <input type="checkbox" class="check" ${lists.completed ? 'checked' : ''}> 
+    <input type="text" id="todo-lists" value = '${lists.description}'>
+    <button class="view"><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>
+    <button class="delete"><i class="fa fa-trash" aria-hidden="true"></i></button>`;
+    taskContainer.appendChild(taskListDiv);
+  });
+};
+
+populateTasks();
